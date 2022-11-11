be933f0ca75a85dfc2bbddb95721b4a2c6cc20fd
Andrés García <andgar2010@gmail.com>
2022-11-11 15:39:50 -0500

    feat: add model local_counter

---
+++ lib/features/counter/data/models/counter_model/local_counter_model.dart
@@ -0,0 +1,44 @@
+import 'dart:convert';
+
+import 'package:http_exception/http_exception.dart';
+
+import '../../../domain/domain.dart';
+
+/// Model class for [LocalCounterModel]
+///
+/// * [count] is the number of times the counter
+class LocalCounterModel {
+  const LocalCounterModel({required this.count});
+
+  factory LocalCounterModel.fromEntity(CounterEntity entity) =>
+      LocalCounterModel(count: entity.count);
+
+  factory LocalCounterModel.fromJson(String sourceJson) {
+    final Map<String, dynamic> counterJson = json.decode(sourceJson);
+    return LocalCounterModel.fromMap(counterJson);
+  }
+
+  factory LocalCounterModel.fromMap(Map<String, dynamic> json) {
+    // var abc = !json.containsKey(<String>['count']);
+    if (!json.keys.toSet().containsAll(<String>['count'])) {
+      throw HttpStatus4xxErrorClient.unprocessableEntity_422.exception(
+        data: json,
+        detail: 'Not found key "count" in JSON',
+      );
+    }
+
+    return LocalCounterModel(count: json['count'].toInt());
+  }
+
+  /// [count] is the number of times the counter
+  final int count;
+
+  @override
+  String toString() => 'LocalCounterModel(count: $count)';
+
+  Map<String, dynamic> toMap() => {'count': count};
+
+  String toJson() => json.encode(toMap());
+
+  CounterEntity toEntity() => CounterEntity(count: count);
+}
---
+++ test/feature/counter/data/models/counter_model/local_counter_model_test.dart
@@ -0,0 +1,81 @@
+import 'package:clean_architecture_counter/features/counter/counter.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:http_exception/http_exception.dart';
+
+void main() {
+  group('Model LocalCounter', () {
+    const LocalCounterModel localCounterModel = LocalCounterModel(count: 1);
+    test('get field count test', () {
+      expect(localCounterModel.count, 1);
+    });
+    test('toEntity() test', () {
+      final CounterEntity counterEntity = localCounterModel.toEntity();
+      expect(counterEntity, isA<CounterEntity>());
+      expect(counterEntity.toString(), 'CounterEntity(count: 1)');
+    });
+    test('toJson() test', () {
+      expect(localCounterModel.toJson(), '{"count":1}');
+    });
+    test('toMap() test', () {
+      expect(localCounterModel.toMap(), {'count': 1});
+    });
+    test("toString() test", () {
+      expect(localCounterModel.toString(), 'LocalCounterModel(count: 1)');
+    });
+    test('factory LocalCounterModel fromEntity() test', () {
+      final LocalCounterModel localCounterModel =
+          LocalCounterModel.fromEntity(const CounterEntity(count: 1));
+      expect(localCounterModel, isA<LocalCounterModel>());
+      expect(localCounterModel.toString(), 'LocalCounterModel(count: 1)');
+    });
+    test('factory LocalCounterModel fromJson() test', () {
+      const String dataJson = '{"count":2000, "countA":1000}';
+      final LocalCounterModel localCounterModel =
+          LocalCounterModel.fromJson(dataJson);
+      expect(localCounterModel, isA<LocalCounterModel>());
+      expect(localCounterModel.toString(), 'LocalCounterModel(count: 2000)');
+    });
+    test('factory LocalCounterModel fromJson() failure test', () {
+      try {
+        const String dataJson = '{"countA":2000, "countB":1000}';
+        LocalCounterModel.fromJson(dataJson);
+      } on HttpException catch (e) {
+        expect(
+            e.toString(),
+            endsWith(
+                'HTTP Status 422 - Unprocessable Entity: Not found key "count" '
+                'in JSON, HTTP data = {countA: 2000, countB: 1000}'));
+        expect(e.message, endsWith('Not found key "count" in JSON'));
+        expect(e.status.toString(), endsWith('422'));
+      }
+    });
+    test('factory LocalCounterModel fromMap() test', () {
+      const Map<String, dynamic> dataMap = {
+        'countA': '1000',
+        'countB': 500,
+        'count': 2000,
+      };
+      final LocalCounterModel localCounterModel =
+          LocalCounterModel.fromMap(dataMap);
+      expect(localCounterModel, isA<LocalCounterModel>());
+      expect(localCounterModel.toString(), 'LocalCounterModel(count: 2000)');
+    });
+    test('factory LocalCounterModel fromMap() failure test', () {
+      try {
+        const Map<String, dynamic> dataMap = {
+          'countA': '1000',
+          'countB': 500,
+        };
+        LocalCounterModel.fromMap(dataMap);
+      } on HttpException catch (e) {
+        expect(
+            e.toString(),
+            endsWith(
+                'HTTP Status 422 - Unprocessable Entity: Not found key "count" '
+                'in JSON, HTTP data = {countA: 1000, countB: 500}'));
+        expect(e.message, endsWith('Not found key "count" in JSON'));
+        expect(e.status.toString(), endsWith('422'));
+      }
+    });
+  });
+}
