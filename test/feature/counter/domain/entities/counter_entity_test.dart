cfd4b2db060b4d57d047ad6e769d67318a6b50cd
Andrés García <andgar2010@gmail.com>
2022-11-11 15:37:33 -0500

    test: entity counter

---
+++ test/feature/counter/domain/entities/counter_entity_test.dart
@@ -0,0 +1,21 @@
+import 'package:clean_architecture_counter/features/counter/counter.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('Entity Counter', () {
+    const CounterEntity counter = CounterEntity(count: 1);
+    test("toString() test", () {
+      expect(counter.toString(), 'CounterEntity(count: 1)');
+    });
+    test("get value of count test", () {
+      expect(counter.count, 1);
+    });
+    test("get props of count test", () {
+      expect(counter.props, [1]);
+    });
+    test("copy for change value of count updated test", () {
+      final CounterEntity counterUpdated = counter.copyWith(count: 2);
+      expect(counterUpdated.count, 2);
+    });
+  });
+}
