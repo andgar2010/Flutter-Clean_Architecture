d4a3be6c2e451c9241cd366d49af8d392b071012
Andrés García <andgar2010@gmail.com>
2022-11-11 15:39:03 -0500

    feat: add model remote_counter

---
+++ lib/features/counter/data/models/counter_model/remote_counter_model.dart
@@ -0,0 +1,68 @@
+import 'dart:convert';
+
+import '../../../domain/entities/counter_entity.dart';
+import '../../../../../core/data/http/http_error.dart';
+
+/// Model class for [RemoteCounterModel]
+///
+/// * [count] is the number of times the counter
+class RemoteCounterModel {
+  const RemoteCounterModel({required this.countA, required this.countB});
+
+  factory RemoteCounterModel.fromEntity(CounterEntity entity) =>
+      RemoteCounterModel(countA: entity.count, countB: 0);
+
+  factory RemoteCounterModel.fromJson(String source) {
+    final Map<String, dynamic> counterJson = json.decode(source);
+    return RemoteCounterModel.fromMap(counterJson);
+  }
+
+  factory RemoteCounterModel.fromMap(Map<String, dynamic> json) {
+    if (!json.keys.toSet().containsAll(<String>['countA', 'countB'])) {
+      throw HttpStatus4xxErrorClient.unprocessableEntity_422.exception(
+        data: json,
+        detail: 'Not found key "countA" and "countB" in JSON',
+      );
+    }
+
+    late int countA;
+    late int countB;
+
+    try {
+      countA = json['countA'].toInt();
+    } catch (_) {
+      countA = int.parse(json['countA']);
+    }
+
+    try {
+      countB = json['countB'].toInt();
+    } catch (_) {
+      countB = int.parse(json['countB']);
+    }
+
+    return RemoteCounterModel(
+      countA: countA,
+      countB: countB,
+    );
+  }
+
+  /// [countA] is the number of times the counterA
+  final int countA;
+
+  /// [countB] is the number of times the counterB
+  final int countB;
+
+  @override
+  String toString() => 'RemoteCounterModel(countA: $countA, countB: $countB)';
+
+  Map<String, dynamic> toMap() {
+    return {
+      'countA': countA,
+      'countB': countB,
+    };
+  }
+
+  String toJson() => json.encode(toMap());
+
+  CounterEntity toEntity() => CounterEntity(count: countA);
+}
---
+++ test/feature/counter/data/models/counter_model/remote_counter_model_test.dart
@@ -0,0 +1,116 @@
+import 'package:clean_architecture_counter/features/counter/counter.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:http_exception/http_exception.dart';
+
+void main() {
+  group('Model RemoteCounter', () {
+    const RemoteCounterModel remoteCounterModel = RemoteCounterModel(
+      countA: 1,
+      countB: 2,
+    );
+
+    test('get field count test', () {
+      expect(remoteCounterModel.countA, 1);
+      expect(remoteCounterModel.countB, 2);
+    });
+    test('toEntity() test', () {
+      final CounterEntity counterEntity = remoteCounterModel.toEntity();
+      expect(counterEntity, isA<CounterEntity>());
+      expect(counterEntity.toString(), 'CounterEntity(count: 1)');
+    });
+    test('toJson() test', () {
+      expect(remoteCounterModel.toJson(), '{"countA":1,"countB":2}');
+    });
+    test('toMap() test', () {
+      expect(remoteCounterModel.toMap(), {'countA': 1, 'countB': 2});
+    });
+    test("toString() test", () {
+      expect(remoteCounterModel.toString(),
+          'RemoteCounterModel(countA: 1, countB: 2)');
+    });
+    test('factory RemoteCounterModel fromEntity() test', () {
+      final RemoteCounterModel remoteCounterModel =
+          RemoteCounterModel.fromEntity(const CounterEntity(count: 1));
+      expect(remoteCounterModel, isA<RemoteCounterModel>());
+      expect(
+        remoteCounterModel.toString(),
+        'RemoteCounterModel(countA: 1, countB: 0)',
+      );
+    });
+    test('factory RemoteCounterModel fromJson() test', () {
+      const String dataJson = '{"countA":2000, "countB":1000}';
+      final RemoteCounterModel remoteCounterModel =
+          RemoteCounterModel.fromJson(dataJson);
+      expect(remoteCounterModel, isA<RemoteCounterModel>());
+      expect(remoteCounterModel.toString(),
+          'RemoteCounterModel(countA: 2000, countB: 1000)');
+    });
+    test('factory RemoteCounterModel fromJson() failure test', () {
+      try {
+        const String dataJson = '{"countA":2000, "count":1000}';
+        RemoteCounterModel.fromJson(dataJson);
+      } on HttpException catch (e) {
+        expect(
+          e.toString(),
+          endsWith(
+              'HTTP Status 422 - Unprocessable Entity: Not found key "countA" '
+              'and "countB" in JSON, HTTP data = {countA: 2000, count: 1000}'),
+        );
+        expect(
+          e.message,
+          endsWith('Not found key "countA" and "countB" in JSON'),
+        );
+        expect(e.status.toString(), endsWith('422'));
+      }
+    });
+    test('factory RemoteCounterModel fromMap() [countA String] test', () {
+      const Map<String, dynamic> dataMap = {
+        'count': 2000,
+        'countA': '1000',
+        'countB': 500,
+      };
+      final RemoteCounterModel remoteCounterModel =
+          RemoteCounterModel.fromMap(dataMap);
+      expect(remoteCounterModel, isA<RemoteCounterModel>());
+      expect(
+        remoteCounterModel.toString(),
+        'RemoteCounterModel(countA: 1000, countB: 500)',
+      );
+    });
+    test('factory RemoteCounterModel fromMap() [countB String] test', () {
+      const Map<String, dynamic> dataMap = {
+        'count': 2000,
+        'countA': 1000,
+        'countB': '500',
+      };
+      final RemoteCounterModel remoteCounterModel =
+          RemoteCounterModel.fromMap(dataMap);
+      expect(remoteCounterModel, isA<RemoteCounterModel>());
+      expect(
+        remoteCounterModel.toString(),
+        'RemoteCounterModel(countA: 1000, countB: 500)',
+      );
+    });
+    test('factory RemoteCounterModel fromMap() failure test', () {
+      try {
+        const Map<String, dynamic> dataMap = {
+          'count': '1000',
+          'countB': 500,
+        };
+        RemoteCounterModel.fromMap(dataMap);
+      } on HttpException catch (e) {
+        expect(
+          e.toString(),
+          endsWith(
+              'HTTP Status 422 - Unprocessable Entity: Not found key "countA" '
+              'and "countB" in JSON, HTTP data = {count: 1000, countB: 500}'),
+        );
+        expect(
+          e.message,
+          endsWith('Not found key "countA" and "countB" in JSON'),
+        );
+        expect(e.status.toString(), endsWith('422'));
+      }
+    });
+  });
+}
