6477d97c1d8b1543cd8f00890ab36f602d89ffb2
Andrés García <andgar2010@gmail.com>
2022-11-11 15:36:09 -0500

    refactor: model to entity counter

---
+++ lib/features/counter/domain/entities/counter_entity.dart
@@ -0,0 +1,20 @@
+import 'package:equatable/equatable.dart';
+
+/// Entry class for [CounterEntity]
+///
+/// * [count] is the number of times the counter
+class CounterEntity extends Equatable {
+  const CounterEntity({required this.count});
+
+  /// [count] is the number of times the counter
+  final int count;
+
+  @override
+  List get props => [count];
+
+  @override
+  String toString() => 'CounterEntity(count: $count)';
+
+  CounterEntity copyWith({int? count}) =>
+      CounterEntity(count: count ?? this.count);
+}
