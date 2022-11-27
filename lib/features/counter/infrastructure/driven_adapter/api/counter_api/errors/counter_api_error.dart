6b956e7da9f6fd776910dbdd1f9a3b89c3f810d5
Andrés García <andgar2010@gmail.com>
2022-11-27 04:15:27 -0500

    feat: add exception of CounterApiError with test

---
+++ lib/features/counter/infrastructure/driven_adapter/api/counter_api/errors/counter_api_error.dart
@@ -0,0 +1,4 @@
+class CounterApiError implements Exception {
+  @override
+  toString() => "Error al obtener valor de un counter";
+}
---
+++ test/feature/counter/infrastructure/driven_adapter/api/errors/counter_api_error_test.dart
@@ -0,0 +1,33 @@
+import 'package:clean_architecture_counter/features/counter/counter.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('Infrastructure - Driven Adapter - API - Error', () {
+    Never throwError() {
+      throw CounterApiError();
+    }
+
+    test('should throw CounterApiError with message', () {
+      expect(throwError, throwsA(isA<CounterApiError>()));
+      expect(
+        throwError,
+        throwsA(
+          isA<CounterApiError>().having(
+            (CounterApiError x) => x.toString(),
+            'message',
+            contains("Error al obtener valor de un counter"),
+          ),
+        ),
+      );
+      expect(
+        throwError,
+        throwsA(
+          predicate((Object? x) {
+            return x is CounterApiError &&
+                x.toString() == "Error al obtener valor de un counter";
+          }),
+        ),
+      );
+    });
+  });
+}
