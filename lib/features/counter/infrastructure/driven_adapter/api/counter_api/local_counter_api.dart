703bdd9242bec5a45aae89a58b8dd89e64e61ec7
Andrés García <andgar2010@gmail.com>
2023-02-27 15:07:01 -0500

    feat: add api driven_adapter local and remote

---
+++ lib/features/counter/infrastructure/driven_adapter/api/counter_api/local_counter_api.dart
@@ -0,0 +1,55 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+import '../../../../data/data.dart';
+import '../../../../domain/domain.dart';
+import 'errors/counter_api_error.dart';
+
+class LocalCounterApi extends CounterGateway {
+  LocalCounterApi(this.client);
+  final http.Client client;
+
+  static const url = 'https://counter.free.beeceptor.com';
+
+  @override
+  Future<CounterEntity> getCount() async {
+    final Uri url = Uri.parse(LocalCounterApi.url);
+
+    try {
+      final http.Response response = await client.get(url);
+      if (response.statusCode != 200) {
+        throw CounterApiError(
+            'Failed to get count: HTTP ${response.statusCode}');
+      }
+      final LocalCounterModel counterDataLocal =
+          LocalCounterModel.fromJson(response.body);
+      final CounterEntity countEntity = counterDataLocal.toEntity();
+      return countEntity;
+    } catch (e) {
+      throw CounterApiError('Failed to get count: $e');
+    }
+  }
+
+  @override
+  Future<List<CounterEntity>> getAllCounts() async {
+    final Uri url = Uri.parse('${LocalCounterApi.url}/counts');
+
+    try {
+      final http.Response response = await client.get(url);
+      if (response.statusCode != 200) {
+        throw CounterApiError(
+            'Network error: Failed to get count: HTTP ${response.statusCode}');
+      }
+      final List countersJson = json.decode(response.body);
+      final List<CounterEntity> countsEntities = [];
+      for (final Map<String, dynamic> counterJson in countersJson) {
+        countsEntities.add(LocalCounterModel.fromMap(counterJson).toEntity());
+      }
+      return countsEntities;
+    } catch (e) {
+      rethrow;
+    }
+  }
+// incrementCounter
+}
---
+++ lib/features/counter/infrastructure/driven_adapter/api/counter_api/remote_counter_api.dart
@@ -0,0 +1,63 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:mocktail/mocktail.dart';
+
+import '../../../../data/data.dart';
+import '../../../../domain/domain.dart';
+import 'errors/counter_api_error.dart';
+
+class MockHttpClient extends Mock implements http.Client {}
+
+class RemoteCounterApi extends CounterGateway {
+  RemoteCounterApi(this.client);
+
+  final http.Client client;
+
+  static const url = 'https://fakestoreapi.com/products';
+
+  @override
+  Future<CounterEntity> getCount() async {
+    final Uri url = Uri.parse('${RemoteCounterApi.url}/products/1');
+
+    try {
+      final http.Response response = await client.get(url);
+      if (response.reasonPhrase != 'OK') {
+        throw CounterApiError(
+            'Failed to get count: HTTP ${response.statusCode}');
+      }
+
+      final RemoteCounterModel counterDataRemote =
+          RemoteCounterModel.fromJson(response.body);
+      final CounterEntity countEntity = counterDataRemote.toEntity();
+      return countEntity;
+    } catch (e) {
+      throw CounterApiError('Failed to get count: $e');
+    }
+  }
+
+  @override
+  Future<List<CounterEntity>> getAllCounts() async {
+    Uri url = Uri.parse(RemoteCounterApi.url);
+
+    try {
+      final http.Response response = await client.get(url);
+      if (response.reasonPhrase != 'OK') {
+        throw CounterApiError(
+            'Network error: Failed to get count: HTTP ${response.statusCode}');
+      }
+
+      final List countersJson = json.decode(response.body);
+      final List<CounterEntity> countsEntities =
+          countersJson.map((counterJson) {
+        assert(counterJson != null);
+        final CounterEntity countEntity =
+            RemoteCounterModel.fromMap(counterJson).toEntity();
+        return countEntity;
+      }).toList();
+      return countsEntities;
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
